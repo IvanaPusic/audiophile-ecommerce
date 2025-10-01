21c1216686bd35c795c080fae7e75483d8074042
Ivana Pusic <ivanapusic@192.168.100.136>
2025-10-01 15:21:56 +0200

    create header and hero for mobile view, import images

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
-export class Header {
-
-}
+export class Header {}
