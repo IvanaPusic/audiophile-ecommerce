6e95b0afb2e38b559473dd8520d57f601bea705d
Ivana Pusic <ivanapusic@192.168.100.136>
2025-10-02 16:18:54 +0200

    create audio gear section and add styling

---
+++ src/app/audio-gear-section/audio-gear-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-gear-section',
+  imports: [],
+  templateUrl: './audio-gear-section.html',
+  styleUrl: './audio-gear-section.scss'
+})
+export class AudioGearSection {
+
+}
